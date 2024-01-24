b9c6168d849f6a9ecfec481f98079e57f0456e6b
seojw4973 <sgw0816.naver.com>
2024-01-24 16:32:29 +0900

    ArithOp.java 사칙연산

---
+++ Ch02_Variable/src/ch02/operator02/ArithOP.java
@@ -0,0 +1,17 @@
+package ch02.operator02;
+
+public class ArithOP {
+
+	public static void main(String[] args) {
+		// 사칙연산
+		int num0 = 10;
+		int num1 = 3;
+		System.out.println(num0+num1);
+		System.out.println(num0-num1);
+		System.out.println(num0*num1);
+		System.out.println(num0/num1);	// 몫
+		System.out.println(num0%num1);	// 나머지
+		
+		
+	}
+}
