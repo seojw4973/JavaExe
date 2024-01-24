b8f5f4b8df1ca78f98e7ed931d2810e1f1a987fb
bitcamp1 <bitcamp1@bitcamp>
2024-01-24 15:37:41 +0900

    DataType.java 변수의 선언

---
+++ Ch02_Variable/src/ch02/variable01/DataType.java
@@ -0,0 +1,22 @@
+package ch02.variable01;
+
+public class DataType {
+	public static void main(String[] args) {
+		// 자료형 변수공간이름 = 값
+		int num = 100;			// 정수
+		double dnum = 3.14;		// 실수
+		char ch = 'a';			// 문자 1글자
+		String name = "홍길동";	// 문자열
+		boolean isRun = true;	// true/false
+		String str = "a";		// 문자열
+		
+		System.out.println(num);
+		num = 999;
+		System.out.println(num);
+		System.out.println(dnum);
+		System.out.println(ch);
+		System.out.println(name);
+		System.out.println(isRun);
+		System.out.println(str);
+	}
+}
