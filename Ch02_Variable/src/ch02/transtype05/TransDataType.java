dcfcf0ec586f634f2c426bec19d3fe08fc510900
seojw4973 <sgw0816.naver.com>
2024-01-25 11:05:36 +0900

    TransDataType 자동변환 예제 3

--- Ch02_Variable/src/ch02/transtype05/TransDataType.java
+++ Ch02_Variable/src/ch02/transtype05/TransDataType.java
@@ -19,6 +19,13 @@ public class TransDataType {
 		
 		int nAvg = (int)avg;
 		System.out.println("평균은 " + nAvg + "입니다.");
+		
+		char ch = 'A';
+		System.out.println(ch);
+		System.out.println((int)ch);
+		System.out.println((int)'a');
+		System.out.println((char)66);
+		System.out.println((char)98);
 	}
 
 }
