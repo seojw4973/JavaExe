8edc6b00ee4c702be655f39c99a5855a97b3f35e
seojw4973 <sgw0816.naver.com>
2024-01-25 17:14:24 +0900

    주석 추가

--- Ch03_Control/src/ch03/for10/ForEx.java
+++ Ch03_Control/src/ch03/for10/ForEx.java
@@ -2,10 +2,10 @@ package ch03.for10;
 
 public class ForEx {
 	public static void main(String[] args) {
-		int i = 1;
-		for(;i<=10;) {
-			System.out.println(i);
-			i++;
+		int i = 1;					//초기
+		for(;i<=10;) {				//조건
+			System.out.println(i);	//실행
+			i++;					//증감
 		}
 	}
 }
