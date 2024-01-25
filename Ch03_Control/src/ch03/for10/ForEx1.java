174cfa0ba443010e75c8a45a945b1ab069e686a8
seojw4973 <sgw0816.naver.com>
2024-01-25 17:14:40 +0900

    ForEx1.java 편리한 for문

---
+++ Ch03_Control/src/ch03/for10/ForEx1.java
@@ -0,0 +1,9 @@
+package ch03.for10;
+
+public class ForEx1 {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 10; i++)  // for문은 (초기; 조건; 증감)의 형태로 사용
+			System.out.println(i);	  // 1번만 실행; 조건(계속비교) ; 실행이 끝나면 변화	
+	}
+
+}
