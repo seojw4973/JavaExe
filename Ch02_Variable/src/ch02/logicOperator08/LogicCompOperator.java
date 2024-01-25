464d4614c1ecc560f42fc4117112ece265b05619
seojw4973 <sgw0816.naver.com>
2024-01-25 11:35:54 +0900

    LogicCompOperator.java 논리/비교 연산자 예제

---
+++ Ch02_Variable/src/ch02/logicOperator08/LogicCompOperator.java
@@ -0,0 +1,27 @@
+package ch02.logicOperator08;
+
+import java.util.Scanner;
+
+public class LogicCompOperator {
+	public static void main(String[] args) {
+		int score;
+		String pass; 
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("당신의 시험 점수는?(0~100) >> ");
+		score = sc.nextInt();
+		System.out.print("당신의 면접의 분위기는?(화목/공격) >> ");
+		pass = sc.next();
+		
+		// And연산은 양쪽 모두 true일때 true 결과 산출
+		// If문은 결과값이 true일 때 {}내부영역의 코드를 실행
+		if(score >= 90 && pass.equals("화목")==true) {
+			System.out.println("당신은 합격입니다.");
+		}else {
+			System.out.println("당신은 더 좋은 곳이 있을거예요");
+		}
+				
+		sc.close();
+	}
+
+}
