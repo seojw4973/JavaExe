f42f91dbe833f21d66e46acad5f47d76f9470162
seojw4973 <sgw0816.naver.com>
2024-01-26 10:55:09 +0900

    Resolve11 문제 해결

---
+++ Ch03_Control/src/ch03/for10/Resolve11.java
@@ -0,0 +1,10 @@
+package ch03.for10;
+
+public class Resolve11 {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 10; i++)  
+			if (i % 2 == 0)
+				System.out.println(i);
+	}
+
+}
