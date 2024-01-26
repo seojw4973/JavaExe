b2f53c906008de174ae4b26ccd5228f05978ba23
seojw4973 <sgw0816.naver.com>
2024-01-26 17:52:55 +0900

    Resolve1,2 최소값 최대값 구하기 문제

---
+++ Ch05_Array/src/ch05/array08/Resolve1.java
@@ -0,0 +1,20 @@
+package ch05.array08;
+
+import java.util.Arrays;
+
+public class Resolve1 {
+	public static void main(String[] args) {
+		int [] num = {91, 50, 73, 89, 71};
+		
+		Arrays.sort(num);
+		for(int i=1;i<num.length;i++) {
+			if(num[0] < num[i])
+		}
+		System.out.println(num[0]);
+		
+		for(int i=3; i<num.length;i--)
+			if(num[4] > num[i])
+		}
+		System.out.println(num[4]);
+
+}
---
+++ Ch05_Array/src/ch05/array08/Resolve2.java
@@ -0,0 +1,10 @@
+package ch05.array08;
+
+public class Resolve2 {
+	public static void main(String[] args) {
+		int [] num = {91, 50, 73, 89, 71};
+		
+		
+	}
+
+}
