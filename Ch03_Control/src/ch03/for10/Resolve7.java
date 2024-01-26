1deb1550ba011c2b340ce056c8360d95ce19f50e
seojw4973 <sgw0816.naver.com>
2024-01-26 10:51:38 +0900

    Resolve7 문제 해결

---
+++ Ch03_Control/src/ch03/for10/Resolve7.java
@@ -0,0 +1,13 @@
+package ch03.for10;
+
+public class Resolve7 {
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			if (i % 2 == 0)
+			System.out.println(i);
+			i++;
+		} while (i<=10);
+	}
+
+}
