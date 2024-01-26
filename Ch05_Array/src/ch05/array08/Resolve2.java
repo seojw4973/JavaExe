4d6d80e605793b74fff84fdad659c45237aa6d84
seojw4973 <sgw0816.naver.com>
2024-01-26 18:02:52 +0900

    Resolve2 문제 해결중

--- Ch05_Array/src/ch05/array08/Resolve2.java
+++ Ch05_Array/src/ch05/array08/Resolve2.java
@@ -1,10 +1,21 @@
 package ch05.array08;
 
+import java.util.Arrays;
+
 public class Resolve2 {
 	public static void main(String[] args) {
-		int [] num = {91, 50, 73, 89, 71};
-		
-		
+		int[] num = { 91, 50, 73, 89, 71 };
+		int i = 0;
+		int j = 0;
+		for (; i < num.length; i++) {
+			if (num[i] <= num[i])
+
+				if (num[j] >= num[i])
+					;
+
+		}
+		System.out.println(num[i]);
+		System.out.println(num[j]);
 	}
 
 }
