3f5963063f156590855aa3d99ddf1fb8c11c8eaf
seojw4973 <sgw0816.naver.com>
2024-01-30 11:45:49 +0900

    Resolve4 수정

--- Ch05_Array/src/ch05/array08/Resolve2.java
+++ Ch05_Array/src/ch05/array08/Resolve2.java
@@ -4,18 +4,20 @@ import java.util.Arrays;
 
 public class Resolve2 {
 	public static void main(String[] args) {
-		int[] num = { 91, 50, 73, 89, 71 };
-		int i = 0;
-		int j = 0;
-		for (; i < num.length; i++) {
-			if (num[i] <= num[i])
+		int[] num = {91, 50, 73, 89, 71};
+		int min, max;
+		min = max = num[0];
+		
+		for (int i = 0; i < num.length; i++) {
+			if (min > num[i]);
+				min = num[i];
 
-				if (num[j] >= num[i])
-					;
-
+				if (max < num[i])
+					max = num[i];
 		}
-		System.out.println(num[i]);
-		System.out.println(num[j]);
+		
+		System.out.println("최솟값은 : " + min);
+		System.out.println("최댓값은 : " + max);
 	}
 
 }
--- Ch05_Array/src/ch05/array08/Resolve4.java
+++ Ch05_Array/src/ch05/array08/Resolve4.java
@@ -3,23 +3,39 @@ package ch05.array08;
 import java.util.Arrays;
 
 public class Resolve4 {
+//	버블정렬 이용
 	public static void main(String[] args) {
-		int[] num = {91, 50, 73, 89, 71};
-		int temp = 0;
+		int[] num = { 91, 50, 73, 89, 71 };
 		
-		System.out.println(Arrays.toString(num));
-		
-		for(int i = 0; i<num.length;i++) {
-			for(int j = i+1; j<num.length;j++) {
-				if(num[i] > num[j]) {
-					temp = num[j];
-					num[j] = num[i];	
-					num[i] = temp;		
+//		int [] num = new int[10000];
+//		
+//		for(int i = 0;i<num.length;i++) {
+//			double dVal = Math.random();
+//			num[i] = (int)(dVal*100000);
+//
+//		System.out.println(Arrays.toString(num));
+
+		for (int i = 0; i < num.length - 1; i++) {
+			for (int j = 0; j < num.length - 1 - i; j++) {
+				if (num[j] > num[j + 1]) {
+					int temp = num[j];
+					num[j] = num[j + 1];
+					num[j + 1] = temp;
 				}
-				
-					
+
 			}
-		}System.out.println(Arrays.toString(num));
+		}
+		System.out.println(Arrays.toString(num));
 	}
 
 }
+//	public static void method0() {
+//		int [] numArr = new int[10000];
+//		
+//		for(int i = 0;i<numArr.length;i++) {
+//			double dVal = Math.random();
+//			numArr[i] = (int)(dVal*100000);
+//		}
+//		System.out.println(Arrays.toString(numArr));
+//	}
+//}
