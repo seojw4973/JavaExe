dc0d4017d163b917f1205eb9bb3dda4a64afe9c5
seojw4973 <sgw0816.naver.com>
2024-01-30 15:10:29 +0900

    Resolve1 문제 해결

--- Ch05_Array/src/ch05/array08/Resolve1.java
+++ Ch05_Array/src/ch05/array08/Resolve1.java
@@ -6,14 +6,16 @@ public class Resolve1 {
 	public static void main(String[] args) {
 		int [] num = {91, 50, 73, 89, 71};
 		
+		System.out.println(Arrays.toString(num));
 		Arrays.sort(num);
-		for (int i = 1; i < num.length; i++) {
-			if (num[0] < num[i])
+		System.out.println(Arrays.toString(num));
+//		for (int i = 1; i < num.length; i++) {
+//			if (num[0] < num[i])
+//
+//				if (num[4] > num[i])
+//					;
 
-				if (num[4] > num[i])
-					;
-
-		}
+		
 		System.out.println(num[0]);
 		System.out.println(num[4]);
 	}
