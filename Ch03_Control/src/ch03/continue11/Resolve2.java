1088bb5074f5a0e2541f12ad99d700b8e5219cd6
seojw4973 <sgw0816.naver.com>
2024-01-30 15:10:08 +0900

    Resolve2, 3 문제 해결

--- Ch03_Control/src/ch03/continue11/Resolve2.java
+++ Ch03_Control/src/ch03/continue11/Resolve2.java
@@ -2,18 +2,25 @@ package ch03.continue11;
 
 public class Resolve2 {
 	public static void main(String[] args) {
-		for(int i = 1; i<=5 ; i++) {
-			if (i == 1)
-				System.out.println("*");
-			else if (i == 2)
-				System.out.println("**");
-			else if (i == 3)
-				System.out.println("***");
-			else if (i == 4)
-				System.out.println("****");
-			else if (i == 5)
-				System.out.println("*****");
+//		for(int i = 1; i<=5 ; i++) {
+//			if (i == 1)
+//				System.out.println("*");
+//			else if (i == 2)
+//				System.out.println("**");
+//			else if (i == 3)
+//				System.out.println("***");
+//			else if (i == 4)
+//				System.out.println("****");
+//			else if (i == 5)
+//				System.out.println("*****");
+//		}
+		for (int i = 0;i<=5;i++) {
+			for(int j = 0;j<=i;j++) {
+				System.out.print('*');
+			}
+			System.out.println();
 		}
+		
 	}
 
 }
--- Ch03_Control/src/ch03/continue11/Resolve3.java
+++ Ch03_Control/src/ch03/continue11/Resolve3.java
@@ -47,46 +47,81 @@ public class Resolve3 {
 //	}
 //
 //}
-// nextLine으로 다시 풀어볼것
-public static void main(String[] args) {
-	String chmon;
-	int dep=0;
-	int wtd=0;
-	int bal = dep - wtd;
-	Scanner sc = new Scanner(System.in);
-	
-	
-	while(true) {
-		System.out.println("---------------------------");
-		System.out.println("1.예금 │ 2.출금 │ 3.잔고 │ 4.종료");
-		System.out.println("---------------------------");
-		System.out.println("선택> ");
-		chmon = sc.nextLine();
-		
-		if (chmon.equals("1")) {
-			System.out.println("예금액> ");
-			dep = sc.nextInt();
-			System.out.println("예금액> " + dep);
+// nextInt로 푼 방법
+	public static void method0() {
+		Scanner sc = new Scanner(System.in);
+
+		boolean isRun = true;
+		int money = 0, m = 0;
+		while (isRun) {
+			System.out.println("---------------------------");
+			System.out.println("1.예금 │ 2.출금 │ 3.잔고 │ 4.종료");
+			System.out.println("---------------------------");
+			System.out.println("선택> ");
+			int sel = sc.nextInt();
+			switch (sel) {
+			case 1:
+				System.out.println("예금액 > ");
+				m = sc.nextInt();
+				money += m;
+				break;
+			case 2:
+				System.out.println("출금액 > ");
+				m = sc.nextInt();
+				money -= m;
+				break;
+			case 3:
+				System.out.println("잔고 > " + money);
+				break;
+			case 4:
+				isRun = false;
+				break;
+			}
 		}
-		else if(chmon.equals("2")) {
-			System.out.println("출금액> ");
-			wtd = sc.nextInt();
-			System.out.println("출금액> " + wtd);
-			
+		System.out.println("프로그램 종료");
+
+		sc.close();
+	}
+
+// nextLine로 푼 방법
+	public static void method1() {
+		Scanner sc = new Scanner(System.in);
+
+		boolean isRun = true;
+		int money = 0;
+		String m;
+		while (isRun) {
+			System.out.println("---------------------------");
+			System.out.println("1.예금 │ 2.출금 │ 3.잔고 │ 4.종료");
+			System.out.println("---------------------------");
+			System.out.print("선택 > ");
+			String sel = sc.nextLine();
+			switch (sel) {
+			case "1":
+				System.out.println("예금액 > ");
+				m = sc.nextLine();
+				money += Integer.parseInt(m);
+				break;
+			case "2":
+				System.out.println("출금액 > ");
+				m = sc.nextLine();
+				money -= Integer.parseInt(m);
+				break;
+			case "3":
+				System.out.println("잔고 > " + money);
+				break;
+			case "4":
+				isRun = false;
+				break;
+			}
 		}
-		else if(chmon.equals("3")) {
-			System.out.println("잔고> ");
-			bal = dep - wtd;
-			System.out.println("잔고> " + bal);
-			
-		}
-		if(chmon.equals("4")) {
-			System.out.println("프로그램 종료");
-			break;
-		}
-		
-	}	sc.close();	
-	
-}
-	
+		System.out.println("프로그램 종료");
+
+		sc.close();
+	}
+
+	public static void main(String[] args) {
+//		method0();
+		method1();
+	}
 }
