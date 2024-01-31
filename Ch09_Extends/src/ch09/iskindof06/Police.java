d41f1ff421284e04fbc0764ac9bb0bbe7214fa1c
seojw4973 <sgw0816.naver.com>
2024-01-31 17:38:36 +0900

    iskindof06.java 상속관계를 설정한 잘못된 예시

---
+++ Ch09_Extends/src/ch09/iskindof06/Police.java
@@ -0,0 +1,19 @@
+package ch09.iskindof06;
+
+public class Police extends Gun{
+
+	private int handCuffs;
+	
+	public Police(int bullet, int handCuffs) {
+		super(bullet);
+		this.handCuffs = handCuffs;
+	}
+	public void putHandCUff() {
+		if(handCuffs > 0) {
+			System.out.println("SNAP!");
+			handCuffs--;
+		}else {
+			System.out.println("Miss...");
+		}
+	}
+}
