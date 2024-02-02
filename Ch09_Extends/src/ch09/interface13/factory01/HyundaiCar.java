ca4e8d4ca7513ab83fe4220b25f11a684c802be1
seojw4973 <sgw0816.naver.com>
2024-02-02 15:53:59 +0900

    FordCar HyundaiCar ToyotaCar 모두 테스트 중간 쉬는 시간 조정

--- Ch09_Extends/src/ch09/interface13/factory01/HyundaiCar.java
+++ Ch09_Extends/src/ch09/interface13/factory01/HyundaiCar.java
@@ -7,25 +7,25 @@ public class HyundaiCar implements ICar {
 	@Override
 	public void turnOn() throws InterruptedException {
 		System.out.println("HyundaiCar - 시동이 부드럽게 잘 걸린다~H");
-		Thread.sleep((long)Math.random()*500+500);
+		Thread.sleep((long)(Math.random()*500)+500);
 	}
 
 	@Override
 	public void turnOff() throws InterruptedException {
 		System.out.println("HyundaiCar - 시동이 부드럽게 OFF~H");
-		Thread.sleep((long)Math.random()*500+500);
+		Thread.sleep((long)(Math.random()*500)+500);
 	}
 
 	@Override
 	public void accel() throws InterruptedException {
 		System.out.println("HyundaiCar - 액셀이 터보로 동작된다~H");
-		Thread.sleep((long)Math.random()*500+500);
+		Thread.sleep((long)(Math.random()*500)+500);
 	}
 
 	@Override
 	public void footBreak() throws InterruptedException {
 		System.out.println("HyundaiCar - 브레이크가 부드럽게 동작된다~H");
-		Thread.sleep((long)Math.random()*500+500);
+		Thread.sleep((long)(Math.random()*500)+500);
 	}
 
 }
