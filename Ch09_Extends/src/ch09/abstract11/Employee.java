e328c2bd64055c72e5d222e7063691c28480583c
seojw4973 <sgw0816.naver.com>
2024-02-02 11:19:43 +0900

    instanceof의 기능

---
+++ Ch09_Extends/src/ch09/abstract11/Employee.java
@@ -0,0 +1,21 @@
+package ch09.abstract11;
+
+/* 다형성(Polymorphism)
+ * 1) 부모 클래스에 자식 객체를 담을 수 있다. (묶어서 관리)
+ * 2) 부모 클래스 변수로 메서드를 호출하면 각 객체의 메서드가 호출된다. (자식의 고유한 개성은 발현)
+ */
+public abstract class Employee {
+	protected String empno;		// 사번
+	protected String name;		// 이름
+	
+	public Employee(String empno, String name) {
+		this.empno = empno;
+		this.name = name;
+	}
+	public void showEmployeeInfo() {
+		System.out.println("-------------------");
+		System.out.println("사번 : " + empno);
+		System.out.println("이름 : " + name);
+	}
+	public abstract double getMonthPay();	// 추상메서드
+}
---
+++ Ch09_Extends/src/ch09/abstract11/EmployeeManager.java
@@ -0,0 +1,118 @@
+package ch09.abstract11;
+
+import java.util.Scanner;
+
+public class EmployeeManager {
+	private final int EMP_NUM = 100;		// 100명의 사원이 최대
+	// Employee의 자식 객체들을 저장
+	private Employee[] empArr = new Employee[EMP_NUM];
+	private int numOfEmp = 0; 	// 저장된 사원객체 수, 다음 사원이 저장될 index
+	private Scanner sc = new Scanner(System.in);
+	
+	private int viewMenu() {
+		System.out.println("[ 사원 선택 ]");
+		System.out.println("1. 정규직");
+		System.out.println("2. 임시직");
+		System.out.println("3. 일용직");
+		System.out.println("4. 전체정보보기");
+		System.out.println("5. 종료");
+		System.out.println("번호 입력 >> ");
+		int sel = sc.nextInt();
+		return sel;
+	}
+	private RegularEmployee createRegularEmployee() {
+		System.out.println("사번 >> ");
+		String empno = sc.next();
+		System.out.println("이름 >> ");
+		String name = sc.next();
+		System.out.println("연봉 >> ");
+		int yearSalary = sc.nextInt();
+		System.out.println("보너스 >> ");
+		int bonus = sc.nextInt();
+		RegularEmployee emp =
+				new RegularEmployee(empno, name, yearSalary, bonus);
+		return emp;
+	}
+	private TempEmployee createTempEmployee() {
+		System.out.println("사번 >> ");
+		String empno = sc.next();
+		System.out.println("이름 >> ");
+		String name = sc.next();
+		System.out.println("연봉 >> ");
+		int yearSalary = sc.nextInt();
+		System.out.println("계약기간 >> ");
+		int hireYear = sc.nextInt();
+		TempEmployee emp =
+				new TempEmployee(empno, name, yearSalary, hireYear);
+		return emp;
+	}
+	private PartTimeEmployee createPartTimeEmployee() {
+		System.out.println("사번 >> ");
+		String empno = sc.next();
+		System.out.println("이름 >> ");
+		String name = sc.next();
+		System.out.println("일당 >> ");
+		int dailyPay = sc.nextInt();
+		System.out.println("일한 일수 >> ");
+		int workDay = sc.nextInt();
+		PartTimeEmployee emp =
+				new PartTimeEmployee(empno, name, dailyPay, workDay);
+		return emp;
+	}
+	private boolean saveEmployee(Employee emp) {
+		boolean isSave = true;
+		
+		if(this.numOfEmp < EMP_NUM) {
+			this.empArr[this.numOfEmp] = emp;
+			this.numOfEmp++;
+			isSave = true;
+		}else {
+			isSave = false;
+		}
+		return true;
+	}
+	private void viewAllEmployeeInfo() {
+		for(int i=0;i<this.numOfEmp;i++) {
+			this.empArr[i].showEmployeeInfo();
+		}
+	}
+	public void run() {
+		boolean isRun = true;
+		while(isRun) {
+			int selNum = viewMenu();
+			Employee emp = null;
+			
+			switch(selNum) {
+			case EmpMenu.REG_EMP:
+				emp = createRegularEmployee();
+				break;
+			case EmpMenu.TEMP_EMP:
+				emp = createTempEmployee();
+				break;
+			case EmpMenu.PART_EMP:
+				emp = createPartTimeEmployee();
+				break;
+			case EmpMenu.ALL_INFO:
+				emp = null;
+				viewAllEmployeeInfo();
+				break;
+			case EmpMenu.EXIT:
+				emp = null;
+				isRun = false;
+				break;
+			default:
+				emp = null;
+				System.out.println("해당 항목이 존재하지 않습니다.");
+				break;
+			}
+			// emp객체가 존재한다면
+			if(emp != null) {
+				boolean isSave = saveEmployee(emp);
+				if(!isSave)
+					System.out.println("더 이상 저장 공간이 없습니다.");
+			}
+		}
+		System.out.println("Program Exit...");
+	}
+	
+}
