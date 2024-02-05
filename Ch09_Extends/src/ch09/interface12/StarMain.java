bb42ddf051935e526a2a60e4bdc461f00dcfe220
seojw4973 <sgw0816.naver.com>
2024-02-05 12:20:12 +0900

    인터페이스 확장

--- Ch09_Extends/src/ch09/interface12/StarMain.java
+++ Ch09_Extends/src/ch09/interface12/StarMain.java
@@ -2,27 +2,29 @@ package ch09.interface12;
 
 public class StarMain {
 	public static void walk(StarUnit[] units) {
-		for(StarUnit unit : units) {
+		for (StarUnit unit : units) {
 			unit.walk();
 		}
+		System.out.println();
 	}
+
 	public static void attack(StarUnit[] units) {
-		for(StarUnit unit : units) {
+		for (StarUnit unit : units) {
 			unit.attack();
 		}
+		System.out.println();
 	}
+
 	public static void die(StarUnit[] units) {
-		for(StarUnit unit : units) {
+		for (StarUnit unit : units) {
 			unit.die();
 		}
+		System.out.println();
 	}
+
 	public static void main(String[] args) {
-		StarUnit[] units = new StarUnit[] {
-				new Marine(),
-				new Scv(),
-				new Hydra()
-		};
-		
+		StarUnit[] units = new StarUnit[] { new Marine(), new Scv(), new Hydra(), new Zealot() };
+
 		walk(units);
 		attack(units);
 		die(units);
