0f1008b2c0d11c193d57435a8418f41a91ecaa2f
seojw4973 <sgw0816.naver.com>
2024-02-05 15:32:21 +0900

    GeneratorThreeNum.java의 getRandomNum() 메서드 구현

--- Ch09_Extends/src/ch09/resolve14/question03/GeneratorThreeNum.java
+++ Ch09_Extends/src/ch09/resolve14/question03/GeneratorThreeNum.java
@@ -1,24 +1,39 @@
 package ch09.resolve14.question03;
 
-/* 0~9사이의 숫자를 랜덤으로 뽑아서
- * 그것을 배열에 저장
-*/
-public class GeneratorThreeNum extends BaseBall {
-	
-
-	public void generatorThreeNum() {
-		for (int i = 0; i < numArr.length; i++) {
-			numArr[i] = (int) (Math.random() * 9 + 1);
-			for (int j = 0; j < i; j++) {
-				if (numArr[j] == numArr[i]) {
-					i--;
-					break;
-				}
-
-			}
+// 난수 3개를 생성하는 역할
+public class GeneratorThreeNum {
+	public static int[] getRandomNum() {
+		int[] numArr = new int[3];
+		
+		// 1번째 난수 생성
+		numArr[0] = (int)(Math.random()*10);	// 0~9사이의 난수 생성
+		
+		// 2번째 난수 생성
+		boolean isRun = true;
+		while(isRun) {
+			int rNum = (int)(Math.random()*10);
+			/* 1번째 값과 같으면 다시 반복해서 다시 생성해라
+			 * */
+			if(rNum == numArr[0]) 
+				continue;
+		
+			numArr[1] = rNum;
+			isRun = false;
 		}
-		for(int x : numArr) {
-			System.out.println(x + " ");
+		
+		// 3번째 난수 생성
+		isRun = true;
+		while(isRun) {
+			int rNum = (int)(Math.random()*10);
+			/* 1번째 값또는 2번째 값과 같으면 다시 반복해서 다시 생성해라
+			 * */
+			if(rNum == numArr[0] || rNum == numArr[1]) 
+				continue;
+		
+			numArr[2] = rNum;
+			isRun = false;
 		}
+		
+		return numArr;
 	}
 }
