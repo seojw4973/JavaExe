e3257d64c1488ddb21c86e934033664891966cdb
seojw4973 <sgw0816.naver.com>
2024-02-06 16:16:39 +0900

    Properties 설명 추가

--- Ch10_Collection/src/ch10/collection01/ch15/sec04/exam03/PropertiesExample.java
+++ Ch10_Collection/src/ch10/collection01/ch15/sec04/exam03/PropertiesExample.java
@@ -2,6 +2,19 @@ package ch10.collection01.ch15.sec04.exam03;
 
 import java.util.Properties;
 
+/* 웹 프로그래밍에서 환경설정 값을 파일에 보관하고
+ * 상황에 따라 바꾼다.
+ * Java 프로그램에서는 이런 환경설정 파일을 읽어서
+ * 처리해야 하는데
+ * 이 때 외부 파일을 주로 *.properties 파일을 사용하고
+ * 이 파일을 읽어들이는 용도로 Properties 클래스를 사용한다.
+ * 
+ * key = value 구조로 되어있다.
+ * 한글은 유니코드 형식으로 변환되기 때문에
+ * *.txt 에 내용을 작성한 후
+ * *.properties 에 붙여넣기 하는 방식으로 주로 사용한다.
+ * */
+
 public class PropertiesExample {
 	public static void main(String[] args) throws Exception {
 		// Properties 컬렉션 생성
