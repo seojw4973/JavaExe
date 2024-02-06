f34c0f887bea6790c89c1797178742e8ca53ee61
seojw4973 <sgw0816.naver.com>
2024-02-06 16:04:27 +0900

    hashMap 주석 추가

--- Ch10_Collection/src/ch10/collection01/ch15/sec04/exam01/HashMapExample.java
+++ Ch10_Collection/src/ch10/collection01/ch15/sec04/exam01/HashMapExample.java
@@ -6,6 +6,19 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
+/* Map은 Dictionary, HashTable 구조라고도 한다.
+ * 검색에 최적화되어 있는 구조이다.
+ * Set은 값 자체를 hash값으로 바꿔서 정렬 후 값을 보관한다.
+ * 그러나 Map은 key값을 hash값으로 바꿔서 정렬 후 값을 보관한다.
+ * 그리고 key hash는 테이블로 만들어 실제 value가 위치한 위치값을 저장한다.
+ * 그래서 key를 넣으면 value를 빠르게 검색할 수 있다.
+ * 아무리 많은 데이터라도 빠르게 검색할 수 있는 자료구조다.
+ * 데이터베이스의 index가 이 자료구조로 되어 있다.
+ * 
+ * '입력 시는 hash값, 정렬이 일어나므로 리스트 구조보다 느리다'
+ * '다만 특정 값 검색은 가장 빠르다'
+ * '전체를 출력하는 것은 리스트 구조가 빠르다'
+ * */
 public class HashMapExample {
 	public static void main(String[] args) {
 		// Map 컬렉션 생성
