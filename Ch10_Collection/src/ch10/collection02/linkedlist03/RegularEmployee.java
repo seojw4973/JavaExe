80cdfe0f2a155e6011c0429a560d68ee09a8647f
seojw4973 <sgw0816.naver.com>
2024-02-07 16:07:35 +0900

    LinkedList로 변환한 배열에 기능 추가하는 문제(아직 못품)

---
+++ Ch10_Collection/src/ch10/collection02/linkedlist03/RegularEmployee.java
@@ -0,0 +1,30 @@
+package ch10.collection02.linkedlist03;
+
+// 정규직 사원
+public class RegularEmployee extends Employee {
+	
+	protected int yearSalary;		// 연봉
+	protected int bonus;			// 보너스
+
+	public RegularEmployee(String empno, String name,
+						int yearSalary, int bonus) {
+		super(empno, name); // Employee(empno, name) 호출
+		this.yearSalary = yearSalary;
+		this.bonus = bonus;
+	}
+
+	@Override
+	public double getMonthPay() {
+		double mPay = (double) (yearSalary+bonus)/12;
+		return mPay;
+	}
+
+	@Override
+	public void showEmployeeInfo() {
+		super.showEmployeeInfo();							// Employee의 show~ 호출
+		System.out.println("연봉 : " + yearSalary);   		// 부모의 기능 + 추가 확장
+		System.out.println("보너스 : " + bonus);				
+		System.out.printf("월급 : %.2f \n", getMonthPay());
+	}
+	
+}
