4b4f8c02e3a6ed4966d6a20389a0cc7feb16338e
seojw4973 <sgw0816.naver.com>
2024-02-08 11:17:45 +0900

    ch13.generic.ch13.sec01 Generic 예제에 주석 추가

--- Ch13_Generic/src/ch13/generic/ch13/sec01/Box.java
+++ Ch13_Generic/src/ch13/generic/ch13/sec01/Box.java
@@ -1,5 +1,10 @@
 package ch13.generic.ch13.sec01;
 
+/* T는 대명사이다
+ * E, A, MY, HELLO ... 모두 가능하다.
+ * T는 자료형을 프로그래머가 직접 선택하라는 의미이다.
+ * 다만 T는 (Type)을 의미한다.
+ * */
 public class Box<T> {
 	public T content;
 }
