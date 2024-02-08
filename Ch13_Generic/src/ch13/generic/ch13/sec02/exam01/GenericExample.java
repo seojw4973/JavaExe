93a9f72ca036ac43bfcb0054a66664ec982b9af5
seojw4973 <sgw0816.naver.com>
2024-02-08 11:23:47 +0900

    출력 코드 추가

--- Ch13_Generic/src/ch13/generic/ch13/sec02/exam01/GenericExample.java
+++ Ch13_Generic/src/ch13/generic/ch13/sec02/exam01/GenericExample.java
@@ -12,6 +12,9 @@ public class GenericExample {
 		// Getter 리턴값은 Tv와 String이 됨
 		Tv tv = product1.getKind();
 		String tvModel = product1.getModel();
+		System.out.println(product1);
+		System.out.println(tv);
+		System.out.println(tvModel);
 		//--------------------------------------------------------------
 		// K는 Car로 대체, M은 String로 대체
 		Product<Car, String> product2 = new Product<>();
@@ -23,5 +26,8 @@ public class GenericExample {
 		// Getter 리턴값은 Car와 String이 됨
 		Car car = product2.getKind();
 		String carModel = product2.getModel();
+		System.out.println(product2);
+		System.out.println(car);
+		System.out.println(carModel);
 	} 
 }
