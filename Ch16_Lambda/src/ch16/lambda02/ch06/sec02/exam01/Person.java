b574484a98612aa33d46e8f7bb70ce6dce571d68
seojw4973 <sgw0816.naver.com>
2024-02-13 18:05:17 +0900

    매개변수가 없는 람다식

---
+++ Ch16_Lambda/src/ch16/lambda02/ch06/sec02/exam01/Person.java
@@ -0,0 +1,7 @@
+package ch16.lambda02.ch06.sec02.exam01;
+
+public class Person {
+	public void action(Workable workable) {
+		workable.work();
+	}
+}
---
+++ Ch16_Lambda/src/ch16/lambda02/ch06/sec02/exam01/Workable.java
@@ -0,0 +1,6 @@
+package ch16.lambda02.ch06.sec02.exam01;
+
+@FunctionalInterface
+public interface Workable {
+	void work();
+}
