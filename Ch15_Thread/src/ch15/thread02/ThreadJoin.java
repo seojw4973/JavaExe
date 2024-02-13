cfa1357999b090f23db5242c8dbb2e0dabd21352
seojw4973 <sgw0816.naver.com>
2024-02-13 14:30:01 +0900

    thread에서의 join()

---
+++ Ch15_Thread/src/ch15/thread02/ThreadJoin.java
@@ -0,0 +1,39 @@
+package ch15.thread02;
+
+public class ThreadJoin {
+	public static void main(String[] args) {
+
+		
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				int sum = 0;
+				for(int i=0;i<10;i++) {
+					sum += i;
+					System.out.println("누적합은 " + sum);
+					
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				System.out.println("총합은 " + sum);
+				Thread workerThread = Thread.currentThread();
+				System.out.println(workerThread.getName() + " 스레드 종료");
+			}
+		});
+		t1.start();
+		
+		try {
+			// VM내의 스레드 종료 신호를 기다리고있다.
+			// run()이 리턴되면 스레드는 종료 신호를 VM에 보낸다.
+			// 종료 신호(Signal)가 오면 join()은 리턴한다.
+			t1.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		Thread mainThread = Thread.currentThread();
+		System.out.println(mainThread.getName() + " 스레드 종료");
+	}
+}
