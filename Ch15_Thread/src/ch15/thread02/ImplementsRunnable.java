754bb44f600021e0c85b141aca2414eace269181
seojw4973 <sgw0816.naver.com>
2024-02-13 12:49:43 +0900

    이미 상속받은 클래스에 스레드 상속시키는 방법

---
+++ Ch15_Thread/src/ch15/thread02/ImplementsRunnable.java
@@ -0,0 +1,83 @@
+package ch15.thread02;
+
+class OwnName{
+	private String ownName;
+
+	public String getOwnName() {
+		return ownName;
+	}
+
+	public void setOwnName(String ownName) {
+		this.ownName = ownName;
+	}
+}
+
+/* ShowNameThread은 이미 OwnName을 상속받았으므로
+ * Thread의 상속을 받을 수 없다.
+ * 이 때는 Runnable을 상속받는다.
+ * */
+class ShowOwnNameThread extends OwnName
+					implements Runnable{
+	
+	public ShowOwnNameThread(String ownName) {
+		this.setOwnName(ownName);
+	}
+	
+	@Override
+	public void run() {
+		for(int i=0;i<100;i++) {
+			System.out.println(i + " 안녕하세요 " + this.getOwnName() + "입니다");
+			
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println(this.getOwnName() + " 스레드 종료");
+	}
+	
+}
+public class ImplementsRunnable {
+	public static void main(String[] args) {
+		// Thread 2개가 vm에 할당되어 준비하고 있는 상태
+		ShowOwnNameThread s1 = new ShowOwnNameThread("철수");
+		ShowOwnNameThread s2 = new ShowOwnNameThread("영희");
+		ShowOwnNameThread s3 = new ShowOwnNameThread("훈이");
+		ShowOwnNameThread s4 = new ShowOwnNameThread("짱구");
+		
+		// 스레드를 동작(run()을 호출)
+		System.out.println("t1 스레드 동작");
+		Thread t1 = new Thread(s1);
+		t1.start();
+		System.out.println("t2 스레드 동작");
+		Thread t2 = new Thread(s2);
+		t2.start();
+		System.out.println("t3 스레드 동작");
+		Thread t3 = new Thread(s3);
+		t3.start();
+		System.out.println("t4 스레드 동작");
+		Thread t4 = new Thread(s4);
+		t4.start();
+		System.out.println("main 스레드 종료");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
