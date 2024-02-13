79d16cdfe347eb787764ed3d50d8e673051dc2a8
seojw4973 <sgw0816.naver.com>
2024-02-13 14:26:04 +0900

    thread에서의 sleep 사용

---
+++ Ch15_Thread/src/ch15/thread02/ThreadSleep.java
@@ -0,0 +1,36 @@
+package ch15.thread02;
+
+public class ThreadSleep {
+	public static void main(String[] args) {
+
+		
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				int sum = 0;
+				for(int i=0;i<10;i++) {
+					sum += i;
+					System.out.println("누적합은 " + sum);
+					
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				System.out.println("총합은 " + sum);
+				Thread workerThread = Thread.currentThread();
+				System.out.println(workerThread.getName() + " 스레드 종료");
+			}
+		});
+		t1.start();
+		
+		try {
+			Thread.sleep(1100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		Thread mainThread = Thread.currentThread();
+		System.out.println(mainThread.getName() + " 스레드 종료");
+	}
+}
