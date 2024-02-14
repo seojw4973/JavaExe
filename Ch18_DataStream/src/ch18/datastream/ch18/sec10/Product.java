60aef2eb5c12f138635fd67c1c7f0c11946a6f5d
seojw4973 <sgw0816.naver.com>
2024-02-14 20:15:15 +0900

    주석 추가

--- Ch18_DataStream/src/ch18/datastream/ch18/sec10/Product.java
+++ Ch18_DataStream/src/ch18/datastream/ch18/sec10/Product.java
@@ -2,6 +2,10 @@ package ch18.datastream.ch18.sec10;
 
 import java.io.Serializable;
 
+// serialVersionUID는 직렬화를 할때 사용
+// serialVersionUID 값을 명시적으로 지정하지 않으면
+// 컴파일러가 계산한 값을 부여하는데 Serializable Class 또는 Outer 클래스에 변경이 있으면
+// serialVersionUID 값이 바뀌게 되고, Serialize 할때와 DeSerialize 할때의 UID 값이 달라져 저장된 값을 객체로 복원할 수 없게 된다.
 public class Product implements Serializable {
 	private static final long serialVersionUID = -621812868470078544L;
 	private String name;
