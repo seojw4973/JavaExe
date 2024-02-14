0efbcafeccd88afcd45fe6ba706951b653ed4941
seojw4973 <sgw0816.naver.com>
2024-02-14 18:48:53 +0900

    Ch18_DataStream 프로젝트 생성 및 예제 작성

---
+++ Ch18_DataStream/src/ch18/datastream/ch18/sec04/exam01/WriteExample.java
@@ -0,0 +1,34 @@
+package ch18.datastream.ch18.sec04.exam01;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+public class WriteExample {
+	public static void main(String[] args) {
+		try {
+			// 문자 기반 출력 스트림 생성
+			Writer writer = new FileWriter("C:/Temp/test.txt");
+			
+			// 1문자씩 출력
+			char a = 'A';
+			writer.write(a);
+			char b = 'B';
+			writer.write(b);
+			
+			// char 배열 출력
+			char[] arr = {'C', 'D', 'E'};
+			writer.write(arr);
+			
+			// 문자열 출력
+			writer.write("FGH");
+			
+			// 버퍼에 잔류하고 있는 문자들을 출력하고, 버퍼를 지움
+			writer.flush();
+			
+			writer.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Ch18_DataStream/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Ch18_DataStream {
+}
